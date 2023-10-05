87e8897bad3f64c3b24509f5c3a3196ffd53a594
Tom <tomasvergara272@gmail.com>
2023-10-05 01:21:22 -0300

    routing

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './pages/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        children:[
+            { path: 'product', component: ProductComponent},
+            { path: '**', redirectTo: 'product'}
+        ]
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
